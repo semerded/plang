a862e3dee4d5f37a2a46db129d160bd470238c62
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-03-27 00:19:03 +0100

    update setup with required packages

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='plang',
     packages=find_packages(exclude=""),
     version='0.1.0',
-    description='plang (aka Pla&g -> Python Library for Apps & Games) is build on top of Pygame 2 (using SDL 2) with highly optimized and user friendly codebase.',
+    description='plang (Python Library for Apps N Games) is build on top of Pygame 2 (using SDL 2) with highly optimized and user friendly codebase.',
     author='Sem Van Broekhoven',
-    install_requires=['pygame'],
+    install_requires=['colorama', 'typing_extensions', 'cffi'],
 )
